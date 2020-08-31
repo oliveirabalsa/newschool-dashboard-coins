5a79dd0f2126b8a6edd34633bfda35f4511e46c3
lucas <lucascardosopsd@gmail.com>
2020-08-31 15:16:51 -0300

    toggle coins flow

--- src/pages/Statement.vue
+++ src/pages/Statement.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <AdminHistory :changesHistory="{ changesHistory }" />
     <balance :Balance="balance" />
+    <input placeholder="CoinQuantity" id="coinQty" :value="0" />
+    <ToggleCoins />
+    <AdminHistory :changesHistory="{ changesHistory }" />
   </div>
 </template>
 
 <script>
 import Balance from "../components/statement/Balance/Balance";
 import AdminHistory from "../components/statement/Admin/AdminHistory";
+import ToggleCoins from "../components/statement/Balance/ToggleCoins";
+import eventBus from "../components/eventBus";
 
 export default {
-  components: { AdminHistory, Balance },
+  components: { AdminHistory, Balance, ToggleCoins },
   data: () => {
     return {
       id: "",
       changesHistory: [],
-      balance: 0
+      balance: 0,
+      countInterval: false
     };
   },
   methods: {
@@ -47,14 +52,34 @@ export default {
           }
         ]
       };
+    },
+
+    addCoins(qty) {
+      //Send Request to server side
+      this.getUserInfo();
+    },
+    removeCoins(qty) {
+      //Send Request to server side
+
+      this.getUserInfo();
     }
   },
   async created() {
     this.id = this.$route.params.id;
     const userInfo = await this.getUserInfo();
 
+    //get userInfo
     this.balance = userInfo.balance;
     this.changesHistory = userInfo.adminchangesHistory;
+
+    //add coins by event
+    eventBus.$on("toggleCoins", type => {
+      const coinQuantity = document.getElementById("coinQty").value;
+
+      type == "add"
+        ? this.addCoins(coinQuantity)
+        : this.removeCoins(coinQuantity);
+    });
   }
 };
 </script>
